97c72bd13cb90e4ea8f20f3473cd35d8d779aaf2
Andriy <admin@Andriy.local>
2021-01-03 12:22:16 +0200

    beta v.1.3

--- js/services.js
+++ js/services.js
@@ -4,33 +4,33 @@ const pathLine = $(".path-line");
 const point = $(".scroll-point");
 
 // control element
-let pointTop = 0;
-let pointLeft = 0;
-let scrollHeight = 0;
+let pointMoving = 0;
+let scrollhandler = 0;
 
 // logic
 $(window).scroll(function () {
     let scroll = $(window).scrollTop();
-    scrollHeight = scroll;
-
     
-    if ( scroll > pathLine.height()){
-        
-        if ( scroll < 1550){
+    console.log(pathLine.position().bottom );
+
+    if ( scroll > pathLine.position().top ){
+
+        if ( scroll > scrollhandler ){
+            console.log(scroll > scrollhandler);
             
-            return point.css({
-                "top": `${pointTop-=5}px`,
-                "left": `${pointLeft-=8}px`
+            point.css({
+                "offset-distance": `${pointMoving += .6}%`
             });
+        } 
 
-        }                             
-        point.css({
-            "top": `${pointTop+=5}px`,
-            "left": `${pointLeft+=8}px`
-        });
-
-        console.log(pointTop, pointLeft);
-
-
+        if ( scroll < scrollhandler ){
+            point.css({
+                "offset-distance": `${pointMoving -= .6}%`
+            });
+        }
     };
+
+    scrollhandler = scroll;
 });
+
+
